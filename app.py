998e2e771f6a464356dcd102078e006b6ab3a608
Sumit Paul <sumit.18.paul@gmail.com>
2023-04-11 23:43:28 +0530

    changes

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def keywords_and_trending_hashtags():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)), host='0.0.0.0')
