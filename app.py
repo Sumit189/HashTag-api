3c468928dfec95f79fd8483b642a887226eee6b9
Sumit <sumit.18.paul@gmail.com>
2023-04-13 17:19:11 +0000

    stopwords

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
 
 app = Flask(__name__)
 
