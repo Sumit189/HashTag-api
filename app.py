1688e8cc361961a701fb78c0803166fb6a4747a3
Sumit <sumit.18.paul@gmail.com>
2023-04-13 16:47:16 +0000

    changes

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
-import spacy
 import os
 from flask import Flask, request, jsonify
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 
-nlp = spacy.load('en_core_web_sm')
-
-def rank_words(a, b):
+def rank_tags(keywords, tags):
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform([*keywords, *tags])
+    keyword_vectors = X[:len(keywords)]
+    tag_vectors = X[len(keywords):]
+    similarity_matrix = cosine_similarity(keyword_vectors, tag_vectors)
     ranked_words = {}
-    for a_word in a:
-        scores = []
-        for b_word in b:
-            score = nlp(a_word).similarity(nlp(b_word))
-            scores.append(score)
-        ranked_words[a_word] = sorted(list(zip(b, scores)), key=lambda x: x[1], reverse=True)[:30]
+    for i, keyword in enumerate(keywords):
+        ranked_words[keyword] = {}
+        for j, tag in enumerate(tags):
+            ranked_words[keyword][tag] = similarity_matrix[i][j]
     return ranked_words
 
-def get_top_words(ranked_words):
-    all_words = []
-    for words in ranked_words.values():
-        all_words.extend([w[0] for w in words])
-    top_words = sorted(set(all_words), key=lambda x: max([w[1] for w in ranked_words.values() if x in [w[0] for w in ranked_words[x]]]), reverse=True)[:30]
-    top_words = [word for word in top_words if max([w[1] for w in ranked_words.values() if word in [w[0] for w in ranked_words[word]]]) >= 0.5]
-    return top_words
-
+def get_top_tags(ranked_words, threshold=0.5, num_tags=30):
+    all_tags = {}
+    for keyword in ranked_words:
+        for tag, score in ranked_words[keyword].items():
+            if tag not in all_tags:
+                all_tags[tag] = 0
+            all_tags[tag] += score
+    top_tags = [tag for tag, score in sorted(all_tags.items(), key=lambda x: x[1], reverse=True) if score >= threshold][:num_tags]
+    return top_tags
 
 @app.route('/', methods=['GET'])
 def hello():
     return 'Hello from API'
 
 @app.route('/getHashtags', methods=['POST'])
-def keywords_and_trending_hashtags():
+def trending_hashtags():
     try:
         api_key = request.headers.get('X-API-Key')
         if not api_key or api_key != os.environ.get('API_KEY'):
@@ -40,30 +46,18 @@ def keywords_and_trending_hashtags():
         if 'keywords' not in data or 'tags' not in data:
             return jsonify({'error': 'Missing Data'}), 400
 
-        ranked_words = rank_words(data['keywords'], data['tags'])
-        trending_hashtags = get_top_words(ranked_words)
+        ranked_words = rank_tags(data['keywords'], data['tags'])
+        trending_hashtags = get_top_tags(ranked_words)
         return jsonify({'trending_hashtags': trending_hashtags}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/extract_product_names', methods=['POST'])
-def extract_product_names_api():
-    try:
-        api_key = request.headers.get('X-API-Key')
-        if not api_key or api_key != os.environ.get('API_KEY'):
-            return jsonify({'error': 'Invalid API key'}), 401
-        
-        data = request.get_json()
-        if 'text' not in data:
-            return jsonify({'error': 'Missing Data'}), 400
-
-        # extract product names from text using spaCy's NER
-        doc = nlp(data['text'])
-        product_names = set([ent.text for ent in doc.ents if ent.label_ == "PRODUCT"])
-
-        return jsonify({'product_names': list(product_names)}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route('/get_keywords', methods=['POST'])
+def extract_keywords(text):
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(text.lower())
+    keywords = [token for token in tokens if token not in stop_words and token.isalpha()]
+    return keywords
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)), host='0.0.0.0')
