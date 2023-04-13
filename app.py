0cca16467d7bb1300ed4078d09a33f9f1bd2115f
Sumit <sumit.18.paul@gmail.com>
2023-04-13 18:07:50 +0000

    punkt

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt')
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
